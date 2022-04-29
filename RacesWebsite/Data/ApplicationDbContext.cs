56df7acf1e91aac5e637fa51097eb621369c8f6b
Ilyana Megy <66316125+IlyanaMegy@users.noreply.github.com>
2022-04-29 12:44:30 +0200

    login

---
+++ RacesWebsite/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RacesWebsite.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
